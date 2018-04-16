edf827c9964929fba6a219cfb442356e13b17dfb
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-16 19:43:17 +0000

    Added more notes near the top

--- file-test.sh
+++ file-test.sh
@@ -3,6 +3,7 @@
 # Notes: the use of the -e switch in the test below checks to see if
 # the file exists and that is it. If you use the -f switch instead
 # it checks to see if it exists and if it is a "regular" file.
+# -s can be used to see if a file is empty or not.
 
 if
 

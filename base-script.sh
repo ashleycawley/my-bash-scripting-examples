65d51aef990c12131e37283145a483438d213514
Ashley Cawley <ash@ashleycawley.co.uk>
2018-05-28 16:50:37 +0000

    init commit

---
+++ base-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Notes
+
+# Header
+echo "##########################################"
+echo "Script Name: $0"
+echo "Bash Version: $BASH_VERSION"
+echo "Author: Ashley Cawley // @ashleycawley"
+echo "##########################################"
+
+# Variables
+
+# Functions
+
+# Main Script

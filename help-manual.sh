1376a2b58627eb1aed30bdffb4e140eb4353eddf
Ashley Cawley <acawley@cloudabove.com>
2019-01-09 13:02:21 +0000

    New script to provide --help functionality

---
+++ help-manual.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Checks the first arguement $1 is not empty AND contains --help or -help
+if [ ! -z "$1" ] && [ `echo $1` == '--help' ] || [ `echo $1` == '-help' ]
+then
+	echo "
+	### Manual / Help ###
+	
+	## Usage:
+	[`whoami`@`hostname`]# migrate domain.co.uk
+	"
+fi

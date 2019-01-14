2e000eeeea021e582a03151e5278abc35761c1b5
Ashley Cawley <ash@ashleycawley.co.uk>
2019-01-14 15:05:48 +0000

    Completed more examples

--- script-name.sh
+++ script-name.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "Script Name: $0"
+# This would display: ./script-name.sh
+echo "$0"
 
+# This would display: script-name.sh
+basename "$0"
+
+# This stores script-name.sh inside the variable $SCRIPTNAME
+SCRIPTNAME=`basename "$0"`
+
+# This example makes use of the variable set above
+echo "The script name is $SCRIPTNAME"

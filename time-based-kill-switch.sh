a250237cdf372831a34e203fbf82c0a8f81d5362
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-01 15:23:43 +0000

    Swapping logic around

--- time-based-kill-switch.sh
+++ time-based-kill-switch.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Time based kill switch
 if [ `date +%s` -gt "1551453824" ]
     then
-        echo "Executing commands!"
+        echo "Date has passed - Exiting with status code 1"
+        exit 1
 fi
 
-echo "Date has passed - Exiting..."
+echo "Executing commands! ..."
+
+ping -c 2 bbc.co.uk
+
 exit 0

aa7e033874f358f7a5894b26fa800b535a48adfa
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-15 22:02:00 +0000

    Cleaning up code & echos

--- self-updating.sh
+++ self-updating.sh
@@ -30,23 +30,17 @@ function MD5_COMPARISON {
 
 MD5_COMPARISON
 
-echo && echo "Comparison check:" && echo
-
 if [ $MY_MD5 != $ONLINE_MD5 ]
 then
-    echo "MD5's are NOT equal!"
-    echo
-    echo "Downloading newer version..."
+    echo "Local & Remote versions are NOT equal. Downloading newer version..." && echo
     wget -q -O $FULLSCRIPTPATH $UPDATE_SOURCE
     chmod +x $FULLSCRIPTPATH
-    echo
+    
     echo "Performing another md5sum check local vs remote..."
-    # Scripts current md5sum hash
-    MY_MD5=(`md5sum $FULLSCRIPTPATH`)
-    # Downloads script from source URL, extracts md5sum and then deletes the temporary file
-    ONLINE_MD5=(`wget -q -O /tmp/testing.md5 $UPDATE_SOURCE; md5sum /tmp/testing.md5 | awk '{print $1}'; rm -f /tmp/testing.md5`)
+    MY_MD5=(`md5sum $FULLSCRIPTPATH`) # Scripts current md5sum hash
+    ONLINE_MD5=(`wget -q -O /tmp/testing.md5 $UPDATE_SOURCE; md5sum /tmp/testing.md5 | awk '{print $1}'; rm -f /tmp/testing.md5`) # DL's script from source, checks md5sum and then deletes temp file
 
     MD5_COMPARISON
 else
-    echo "MD5's are equal :-)"
+    echo "Local program is already up to date with the remote repository."
 fi

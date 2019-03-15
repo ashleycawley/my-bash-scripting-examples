b35b5824571f811bb167882700de3912d1b0acb4
Ashley Cawley <ash@ashleycawley.co.uk>
2019-03-15 21:03:24 +0000

    Should now work

--- self-updating.sh
+++ self-updating.sh
@@ -8,7 +8,7 @@ SCRIPTNAME=`basename "$0"`
 # Storing the full path and filename in a variable:
 FULLSCRIPTPATH="`pwd`/$SCRIPTNAME"
 
-UPDATE_SOURCE=""
+UPDATE_SOURCE="https://raw.githubusercontent.com/ashleycawley/my-bash-scripting-examples/master/self-updating.sh"
 
 # URL to check to see if update should proceed
 2FA="http://status.ashleycawley.co.uk/update-2fa.txt"
@@ -16,10 +16,14 @@ UPDATE_SOURCE=""
 # Scripts current md5sum hash
 MY_MD5=(`md5sum $FULLSCRIPTPATH`)
 
+# Downloads script from source URL, extracts md5sum and then deletes the temporary file
+ONLINE_MD5=(`wget -q -O /tmp/testing.md5 $UPDATE_SOURCE; md5sum /tmp/testing.md5 | awk '{print $1}'; rm -f /tmp/testing.md5`)
+
+
 # Script
 
 echo "Script's current md5: $MY_MD5
-
 "
 
-echo "Scripts online md5 at $UPDATE_URL"
+echo "Script's online md5 at $ONLINE_MD5
+"

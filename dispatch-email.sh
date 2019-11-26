dda1c7018f60e51581b2a28c902e79f8e101af08
Ashley Cawley <ash@ashleycawley.co.uk>
2019-11-26 09:16:13 +0000

    added notes about installing mailx

--- dispatch-email.sh
+++ dispatch-email.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# If you receive an error saying "mail" is not installed when trying to use this
+# then you may need to install:
+# yum install mailx
+# apt-get install mailx
+
 # Emails the contents of file.txt to the email address shown
 cat file.txt | mail -s "Subject" email@address.com

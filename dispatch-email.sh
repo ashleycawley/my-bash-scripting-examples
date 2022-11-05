b07e2022c6b56947fbae93731a45ba55a1ca4846
Ashley Cawley <ash@ashleycawley.co.uk>
2022-11-05 17:26:18 +0000

    Update dispatch-email.sh

--- dispatch-email.sh
+++ dispatch-email.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# If you receive an error saying "mail" is not installed when trying to use this
-# then you may need to install:
+# If you receive an error saying "mail" is not installed when trying to use this then you may need to install:
 # yum install mailx
 # apt-get install mailx
+# ^^ That used to work on older systems, but now newer Raspbian systems might require a combo of these instead:
+# bsd-mailx postfix
 
 # Emails the contents of file.txt to the email address shown
 cat file.txt | mail -s "Subject" email@address.com
